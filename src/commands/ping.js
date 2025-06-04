2f4f9c811b7296ca5cb579208eb87aa1ae58039f
nicetrykiddo <bestxd916@gmail.com>
2025-06-04 08:41:02 +0530

    Fix logging directory creation and valid ping command

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,3 +1,7 @@
-module.exports = * @param {String} message - The text to be sent in this call (ex: "/telegrapher") 
-
- */		   function genPing(message)  { return JSON_stringify({'cmd': 'chat', "text": "'" + escapeJSONValue($('#msg').val())+"'}); } // sends out our own chat pings via telegram and returns them back, with no additional context information on what was requested by someone else if they said we were able -- just like `/bot` does not have an equivalent of that! Will also print some details about those things so far.. [TODO] Send more info here? Is it worth it anyway?! /p
+module.exports = async function ping(ctx) {
+  try {
+    await ctx.reply('pong');
+  } catch (err) {
+    console.error('Ping command error:', err.message);
+  }
+};
